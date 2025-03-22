e1b9050c20624fe38f91c04ce88050e0f177f99b
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-03-22 18:36:03 +0100

    Added a DatabaseContext corresponding to the Class ApplicationUser

---
+++ Data/ApplicationUserContext.cs
@@ -0,0 +1,13 @@
+﻿using Finance_Monitor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_Monitor.Data
+{
+    public class ApplicationUserContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationUserContext(DbContextOptions<ApplicationUserContext> options) : base(options)
+        {
+        }
+    }
+}
