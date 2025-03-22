67022737cc8f262c400eba47e8542b6ac6279c9d
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-03-22 18:43:25 +0100

    Modified the Class ApplicationUser to add a FirstName parameter, besides the existing Name parameter. Also added documentation.

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,14 @@ namespace Finance_Monitor.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Gets or sets the name for this user.
+        /// </summary>
         public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the first name for this user.
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
     }
 }
