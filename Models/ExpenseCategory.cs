8784dba18e4ef31aaa83c283bb2a511bf1f9d375
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-03-30 17:08:55 +0200

    Created a model Class for the ExpenseCategories.

---
+++ Models/ExpenseCategory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Finance_Monitor.Models
+{
+    public class ExpenseCategory
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+    }
+}
