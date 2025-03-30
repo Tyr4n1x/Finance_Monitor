ab58813b3673c436c7252ac9294f40bddf5c6b6f
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-03-30 13:37:44 +0200

    Created a model Class for the Incomes.

---
+++ Models/Income.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Finance_Monitor.Models
+{
+    public class Income
+    {
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter an amount greater than {1}")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public List<IncomeCategory> Categories { get; set; } = new List<IncomeCategory>();
+
+    }
+
+    // Placeholder for testing purposes, should be replaced with a customizable solution
+    public enum IncomeCategory
+    {
+        Salary,
+        CapitalGain,
+        Freelancing,
+        Coaching,
+        Flipping,
+        Gift,
+    }
+}
+
+
