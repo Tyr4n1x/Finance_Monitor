1309f1d40f1dec1fe58acab4af42e727337f80ac
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-04-02 20:36:57 +0200

    Modified the toString() method of the Class YearlyItem

--- Services/YearlyItem.cs
+++ Services/YearlyItem.cs
@@ -1,8 +1,15 @@
-﻿namespace Finance_Monitor.Services
+﻿using System.Globalization;
+
+namespace Finance_Monitor.Services
 {
     public class YearlyItem
     {
         public required string Month { get; set; }
         public decimal Amount { get; set; }
+        public override string ToString()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            return $"{Month}: {Amount:C2}"; // Formats the amount as currency
+        }
     }
 }
