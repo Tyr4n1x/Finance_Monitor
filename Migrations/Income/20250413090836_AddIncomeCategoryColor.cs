83231061c3f581a706db949e4515dc6a8614adf1
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-04-13 11:14:57 +0200

    Migrated the IncomeContext with the new definition of the Model IncomeCategory. Updated the database accordingly.

---
+++ Migrations/Income/20250413090836_AddIncomeCategoryColor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finance_Monitor.Migrations.Income
+{
+    /// <inheritdoc />
+    public partial class AddIncomeCategoryColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ColorHex",
+                table: "IncomeCategories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorHex",
+                table: "IncomeCategories");
+        }
+    }
+}
