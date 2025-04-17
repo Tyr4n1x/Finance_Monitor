ba5b1aecc91134da42b6dfc7523c3d8f16b1673f
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-04-17 20:47:50 +0200

    Modified the Existing FinanceState to include a new event OnCategoryAdded, that should be triggered when a new Category is defined.

--- Services/FinanceState.cs
+++ Services/FinanceState.cs
@@ -3,12 +3,21 @@
     public class FinanceState
     {
         public event Func<Task>? OnFinanceAdded;
+        public event Func<Task>? OnCategoryAdded;
 
         public async Task NotifyFinanceAdded()
         {
             if (OnFinanceAdded != null)
             {
                 await OnFinanceAdded.Invoke();
+            }
+        }
+
+        public async Task NotifyCategoryAdded()
+        {
+            if (OnCategoryAdded != null)
+            {
+                await OnCategoryAdded.Invoke();
             }
         }
     }
