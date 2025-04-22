c4e7ba97dfe0628acbdeaf55bbc62a4b0c2f7491
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-04-22 21:32:54 +0200

    Modified the IDataService to incorporate two new methods for retrieving the Categories linked to a specific user.

--- Services/IDataService.cs
+++ Services/IDataService.cs
@@ -1,4 +1,6 @@
-﻿namespace Finance_Monitor.Services
+﻿using Finance_Monitor.Models;
+
+namespace Finance_Monitor.Services
 {
     public interface IDataService
     {
@@ -6,5 +8,7 @@
         Task<ICollection<YearlyItem>> LoadCurrentYearIncomes(string userId);
         Task<MonthlyData> LoadCurrentMonthExpenses(string userId);
         Task<MonthlyData> LoadCurrentMonthIncomes(string userId);
+        Task<List<ICategory>> GetExpenseCategories(string userId);
+        Task<List<ICategory>> GetIncomeCategories(string userId);
     }
 }
