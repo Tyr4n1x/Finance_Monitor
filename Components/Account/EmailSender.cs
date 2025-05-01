29704d7bf344830b30f59a0093216625d863c253
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-05-01 15:39:53 +0200

    Implement IEmailSender based on the SendGrid API.

---
+++ Components/Account/EmailSender.cs
@@ -0,0 +1,66 @@
+﻿using Finance_Monitor.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Finance_Monitor.Components.Account
+{
+    public class EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+    ILogger<EmailSender> logger) : IEmailSender<ApplicationUser>
+    {
+        private readonly ILogger logger = logger;
+
+        public AuthMessageSenderOptions Options { get; } = optionsAccessor.Value;
+
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email,
+            string confirmationLink) => SendEmailAsync(email, "Confirm your email",
+            "<html lang=\"en\"><head></head><body>Please confirm your account by " +
+            $"<a href='{confirmationLink}'>clicking here</a>.</body></html>");
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email,
+            string resetLink) => SendEmailAsync(email, "Reset your password",
+            "<html lang=\"en\"><head></head><body>Please reset your password by " +
+            $"<a href='{resetLink}'>clicking here</a>.</body></html>");
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email,
+            string resetCode) => SendEmailAsync(email, "Reset your password",
+            "<html lang=\"en\"><head></head><body>Please reset your password " +
+            $"using the following code:<br>{resetCode}</body></html>");
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            if (string.IsNullOrEmpty(Options.SendGridKey))
+            {
+                throw new Exception("Null SendGridKey");
+            }
+
+            await Execute(Options.SendGridKey, subject, message, toEmail);
+        }
+
+        public async Task Execute(string apiKey, string subject, string message,
+            string toEmail)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(Options.fromEmail),
+                Subject = subject,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(toEmail));
+            var response = await client.SendEmailAsync(msg);
+
+            if (response.IsSuccessStatusCode)
+            {
+                logger.LogInformation("Email to {EmailAddress} sent!", toEmail);
+            }
+            else
+            {
+                var responseBody = await response.Body.ReadAsStringAsync();
+                logger.LogError("Email to {EmailAddress} failed with status code {StatusCode}, Response: {ResponseBody}",
+                    toEmail, response.StatusCode, responseBody);
+            }
+        }
+    }
+}
