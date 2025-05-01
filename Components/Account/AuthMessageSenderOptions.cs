a20a0649c954bb1e21c9641cbb239b10a69b15c5
BenDeSchampheleire <99195628+BenDeSchampheleire@users.noreply.github.com>
2025-05-01 15:38:20 +0200

    In order to select and configure an email provider, create a class AuthMessageSenderOptions to hold the secret email provider API key and sender email address.

---
+++ Components/Account/AuthMessageSenderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Finance_Monitor.Components.Account
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey { get; set; }
+        public string? fromEmail { get; set; }
+    }
+}
